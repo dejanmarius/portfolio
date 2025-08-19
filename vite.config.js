cafed70b86605e37ca5c9d095259c6fca8f4ad21
dejanmarius <mariusdejan6@gmail.com>
2025-08-19 15:42:19 +0300

    message

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // replace with your GitHub repository name
   plugins: [react(), tailwindcss()],
 })
